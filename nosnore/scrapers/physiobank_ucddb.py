8b5ed03e77c505195c16cc4c898f6ce7be66c416
ana-balica <ana.balica@gmail.com>
2014-04-05 12:14:30 +0200

    add docstring to the ucddb scraper

--- nosnore/scrapers/physiobank_ucddb.py
+++ nosnore/scrapers/physiobank_ucddb.py
@@ -6,6 +6,11 @@ from lxml import etree
 
 
 def download(url):
+    """ Download contents of a page
+
+    :param url: target URL where from to download contents
+    :return: string page contents
+    """
     browser = urllib.urlopen(url)
     response = browser.getcode()
     if response == 200:
@@ -17,6 +22,9 @@ def download(url):
 
 
 def scrape_ucddb():
+    """ Scrape recorded data from UCD database and save all the files to the 
+    filesystem
+    """
     BASE_URL = "http://physionet.org/physiobank/database/ucddb/"
     contents = download(BASE_URL)
     tree = etree.HTML(contents)
@@ -29,6 +37,11 @@ def scrape_ucddb():
 
 
 def filter_urls(urls):
+    """ Filter the extracted URLs by choosing only *.txt and *.rec files
+
+    :param urls: a list of url strings
+    :return: a list of filtered url strings
+    """
     filtered_urls = []
     for url in urls:
         if url.endswith("txt") or url.endswith("rec"):
@@ -37,6 +50,10 @@ def filter_urls(urls):
 
 
 def save_contents(url):
+    """ Save the contents of a page to a file
+
+    :param url: string URL 
+    """
     contents = download(url)
     SAVE_PATH = "../samples/ucddb/"
     try:
@@ -48,7 +65,6 @@ def save_contents(url):
     with open(os.path.join(SAVE_PATH, filename), "wb") as f:
         f.write(contents)
     print "File '{0}' saved".format(filename)
-
 
 
 if __name__ == '__main__':
