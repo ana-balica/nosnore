c0b874ae6d51a707e417698e7015cb326e551de6
ana-balica <ana.balica@gmail.com>
2014-04-06 15:09:55 +0200

    refactor plot_uccddb functions

--- nosnore/core/plot_uccddb.py
+++ nosnore/core/plot_uccddb.py
@@ -4,33 +4,14 @@ import pylab as pl
 import scipy.io as sio
 
 
-def plot_signal(name):
-    """ Plot a x,y graph that shows a sound signal
+def get_singal_info(name):
+    """Extract signal information
 
-    :param name: filename of the signal to be processed
-    """
-    info_name = name + ".info"
-    mat_name = name + ".mat"
-
-    with open(info_name, "r") as f:
-        info_lines = f.readlines()
-    interval, gain, base, units = parse_info(info_lines)
-    mat_data = get_mat_data(mat_name, base, gain)
-    x = numpy.arange(0, mat_data.shape[1]) * interval
-
-    pl.plot(x, mat_data[0])
-    pl.xlabel('Time (sec)')
-    pl.ylabel('Sound signal ({0})'.format(units))
-    pl.show()
-
-
-def parse_info(info_lines):
-    """ Extract signal information
-
-    :param info_lines: array of strings representing lines of text from signal
-                       info file
+    :param name: name of file that contains singal info
     :return: tuple of extracted data (signal interval, base, gain, units)
     """
+    with open(name, "r") as f:
+        info_lines = f.readlines()
     interval_regex = re.search(r"(\d+\.\d+)\Wsec", info_lines[3])
     interval = float(interval_regex.group(1))
     signal_data = re.split(r"\s", info_lines[5])
@@ -41,7 +22,7 @@ def parse_info(info_lines):
 
 
 def get_mat_data(mat_name, base, gain):
-    """ Read and preprocess matlab data
+    """Read and preprocess matlab data
 
     :param mat_name: the name of the file that holds mat data
     :param base: base of the signal
@@ -56,5 +37,58 @@ def get_mat_data(mat_name, base, gain):
     return mat_data
 
 
+def get_timewave_data(name):
+    """Compute and return timewave data - signal amplitude and time intervals
+
+    :param name: filename of the signal to be processed
+    """
+    info_name = name + ".info"
+    mat_name = name + ".mat"
+
+    interval, gain, base, units = get_singal_info(info_name)
+    mat_data = get_mat_data(mat_name, base, gain)
+    x = numpy.arange(0, mat_data.shape[1]) * interval
+    return mat_data, x
+
+
+def plot_time_signal(name):
+    """Plot a x,y graph of a sound time-expanded waveform
+
+    :param name: filename of the signal to be ploted
+    """
+    mat_data, x = get_timewave_data(name)
+
+    pl.plot(x, mat_data[0])
+    pl.xlabel('Time (sec)')
+    pl.ylabel('Sound signal (nd)')
+    pl.show()
+
+
+def plot_frequency_signal(name):
+    """Plot x,y graph of a sound signal in frequency domain
+
+    :param name: filename of the signal to be processed
+    """
+    info_name = name + ".info"
+    mat_name = name + ".mat"
+
+    with open(info_name, "r") as f:
+        info_lines = f.readlines()
+    interval, gain, base, units = parse_info(info_lines)
+    mat_data = get_mat_data(mat_name, base, gain)
+    x = numpy.arange(0, mat_data.shape[1]) * interval
+
+    print "starting autocorrelation"
+    print mat_data[0][:3]
+    cor_mat_data = numpy.correlate(mat_data[0][:100], mat_data[0][:100], "same")
+    print len(cor_mat_data)
+    print x.shape
+    pl.plot(x[:100], cor_mat_data)
+    pl.xlabel('Time (sec)')
+    pl.ylabel('Sound signal ACF')
+    # pl.show()
+
+
 if __name__ == '__main__':
-    plot_signal('../samples/ucddb/short_sound_records/ucddb003_recm')
+    plot_time_signal('../samples/ucddb/short_sound_records/ucddb003_recm')
+    # plot_frequency_signal('../samples/ucddb/short_sound_records/ucddb003_recm')
