c084ac4e154eb8472fa004653d73844b3f5c8e95
ana-balica <ana.balica@gmail.com>
2014-04-25 16:22:37 +0200

    plot some sample figures: signal, fft, envelope

--- nosnore/core/process_pcp.py
+++ nosnore/core/process_pcp.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import pylab as pl
 from scipy.io import wavfile
-from nosnore.core.dsp import autocorrelate, compute_psd
+from nosnore.core.dsp import highpassfilter, autocorrelate, compute_psd, get_envelope
 
 
 def getwavdata(filename):
@@ -32,6 +32,25 @@ def save_plot(data, name, x_vals=None):
     pl.savefig(name)
 
 
+def plot_some_signals():
+    f = "nosnore/samples/cvut/01pcp_data.wav"
+    rate, data = getwavdata(f)
+    n = data.size
+    time = np.linspace(0, n/rate, num=n)
+    chunks = make_chunks(data, 70000)
+    for i, chunk in enumerate(chunks):
+        # save the chunk in the time domain
+        save_plot(chunk, str(i+1)+"snore.png", time[:chunk.size])
+        autocorr = autocorrelate(chunk)
+        x_vals, datafft = compute_psd(autocorr, time)
+        # save the chunk after fft and autocorrelation
+        save_plot(datafft, str(i+1)+"snore_fft.png", x_vals)
+        # save the envelope of the signal
+        env = get_envelope(chunk)
+        save_plot(env, str(i+1)+"signal_envelope.png", time[:env.size])
+        if i == 9:
+            break
+
 if __name__ == '__main__':
     files = ["nosnore/samples/cvut/01pcp_data.wav",
              "nosnore/samples/cvut/02pcp_data.wav",
@@ -40,13 +59,3 @@ if __name__ == '__main__':
              "nosnore/samples/cvut/05pcp_data.wav",
              "nosnore/samples/cvut/06pcp_data.wav"]
 
-    rate, data = getwavdata(files[0])
-    n = data.size
-    time = np.linspace(0, n/rate, num=n)
-    chunks = make_chunks(data, 70000)
-    for i, chunk in enumerate(chunks):
-        autocorr = autocorrelate(chunk)
-        x_vals, datafft = compute_psd(chunk, time[:chunk.size])
-        # save_plot(datafft, str(i+1)+"snore.png")
-        # if i == 10:
-        #     break
