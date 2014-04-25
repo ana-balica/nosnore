66326db475947590d258ad6f29bc7a9fad90bf5c
ana-balica <ana.balica@gmail.com>
2014-04-25 11:59:50 +0200

    process pcp data by chunks
    PCP - Prague Psychiatric Center

--- nosnore/core/process_pcp.py
+++ nosnore/core/process_pcp.py
@@ -30,3 +30,23 @@ def save_plot(data, name, x_vals=None):
         pl.plot(data)
     pl.grid()
     pl.savefig(name)
+
+
+if __name__ == '__main__':
+    files = ["nosnore/samples/cvut/01pcp_data.wav",
+             "nosnore/samples/cvut/02pcp_data.wav",
+             "nosnore/samples/cvut/03pcp_data.wav",
+             "nosnore/samples/cvut/04pcp_data.wav",
+             "nosnore/samples/cvut/05pcp_data.wav",
+             "nosnore/samples/cvut/06pcp_data.wav"]
+
+    rate, data = getwavdata(files[0])
+    n = data.size
+    time = np.linspace(0, n/rate, num=n)
+    chunks = make_chunks(data, 70000)
+    for i, chunk in enumerate(chunks):
+        autocorr = autocorrelate(chunk)
+        x_vals, datafft = compute_psd(chunk, time[:chunk.size])
+        # save_plot(datafft, str(i+1)+"snore.png")
+        # if i == 10:
+        #     break
