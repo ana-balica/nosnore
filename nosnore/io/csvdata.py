b534e5141d617ba50fff636788faeeb6a2bbd402
ana-balica <ana.balica@gmail.com>
2014-04-28 12:09:45 +0200

    read feature rows in io module

--- nosnore/io/csvdata.py
+++ nosnore/io/csvdata.py
@@ -11,3 +11,19 @@ def add_rows(filename, rows):
     with open(filename, "a") as f:
         writer = csv.writer(f, delimiter=",")
         writer.writerows(rows)
+
+
+def read_feature_rows(filename, sid, ica=False):
+    with open(filename, "rb") as f:
+        reader = csv.reader(f, delimiter=",")
+        features = []
+        for row in reader:
+            if int(row[0]) == int(sid):
+                if ica is False:
+                    if row[3] == "False":
+                        features.extend([float(row[2])])
+                else:
+                    if row[3] == "True":
+                        features.extend([float(row[2])])
+
+        return features
