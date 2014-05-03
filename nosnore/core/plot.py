dc234bfe162b4e1cf54626ba276d3429a72576f8
ana-balica <ana.balica@gmail.com>
2014-05-03 11:09:54 +0200

    save a whole plot of a signal

--- nosnore/core/plot.py
+++ nosnore/core/plot.py
@@ -1,6 +1,16 @@
 import pylab as pl
 
 
+def show_plot(data, x_vals=None):
+    pl.clf()
+    if x_vals is not None:
+        pl.plot(x_vals, data)
+    else:
+        pl.plot(data)
+    pl.grid()
+    pl.show()
+
+
 def save_plot(data, name, x_vals=None):
     pl.clf()
     if x_vals is not None:
@@ -11,11 +21,17 @@ def save_plot(data, name, x_vals=None):
     pl.savefig(name)
 
 
-def show_plot(data, x_vals=None):
+def save_signal_plots(signal, name):
     pl.clf()
-    if x_vals is not None:
-        pl.plot(x_vals, data)
-    else:
-        pl.plot(data)
+    pl.figure(1)
+    pl.subplot(311)
+    pl.plot(signal.time, signal.signal)
     pl.grid()
-    pl.show()
+    pl.subplot(312)
+    pl.plot(signal.autocorr)
+    pl.grid()
+    pl.subplot(313)
+    pl.plot(*signal.psd_)
+    pl.grid()
+
+    pl.savefig(name)
