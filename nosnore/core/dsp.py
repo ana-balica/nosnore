e1caead5b7c4ca37a52c8166b9f298a12ed606ad
ana-balica <ana.balica@gmail.com>
2014-05-04 19:26:33 +0200

    change detect_formants function

--- nosnore/core/dsp.py
+++ nosnore/core/dsp.py
@@ -78,46 +78,68 @@ def smooth(signal, window_size, order, deriv=0, rate=1):
     return np.convolve( m[::-1], signal, mode='valid')
 
 
-def detect_formants(signal, delta):
+def detect_formants(y_axis, x_axis=None, lookahead = 200, delta=0):
     """Detect formants (peaks) in a vector.
-    A point will be considered a formant if it has the maximal value and is 
-    preceded to the left by a value lower than delta.
-    Original source - https://gist.github.com/endolith/250860
+    A point will be considered a formant if it has the maximal or minimal value 
+    and is preceded to the left by a value lower/larger than delta.
+    Original source - https://gist.github.com/sixtenbe/1178136
 
-    :param signal: numpy 1D array values of the time varying signal
-    :param delta: 
+    :param y_axis: numpy 1D array values of the time varying signal
+    :param x_axis: numpy 1D array whose values correspond to the y_axis array and
+                   is used in the return to specify the position of the formants.
+    :param lookahead: int distance to look ahead from a formant candidate to
+                      determine if it's an actual formants
+    :param delta: int minimal difference between a formant and the following points
+    :return: tuple of 2 lists containing the positive and the negative formants. 
+             Each item of the list contains a tuple (position, formant_value)
     """
-    maxtab = []
-    mintab = []
+    max_peaks = []
+    min_peaks = []
+    dump = []
 
-    if delta <= 0:
-        raise ValueError('Input argument delta must be a positive scalar')
+    length = len(y_axis)
+    
+    if lookahead < 1:
+        raise ValueError, "Lookahead must be '1' or above in value"
+    if not (np.isscalar(delta) and delta >= 0):
+        raise ValueError, "delta must be a positive number"
     
     mn, mx = np.Inf, -np.Inf
-    mnpos, mxpos = np.NaN, np.NaN
     
-    lookformax = True
-    x = np.arange(len(signal))
-    for i in np.arange(len(signal)):
-        this = signal[i]
-        if this > mx:
-            mx = this
-            mxpos = x[i]
-        if this < mn:
-            mn = this
-            mnpos = x[i]
+    for index, (x, y) in enumerate(zip(x_axis[:-lookahead], y_axis[:-lookahead])):
+        if y > mx:
+            mx = y
+            mxpos = x
+        if y < mn:
+            mn = y
+            mnpos = x
         
-        if lookformax:
-            if this < mx-delta:
-                maxtab.append((mxpos, mx))
-                mn = this
-                mnpos = x[i]
-                lookformax = False
+        if y < mx-delta and mx != np.Inf:
+            if y_axis[index:index+lookahead].max() < mx:
+                max_peaks.append((mxpos, mx))
+                dump.append(True)
+                mx = np.Inf
+                mn = np.Inf
+                if index+lookahead >= length:
+                    break
+                continue
+        
+        if y > mn+delta and mn != -np.Inf:
+            if y_axis[index:index+lookahead].min() > mn:
+                min_peaks.append((mnpos, mn))
+                dump.append(False)
+                mn = -np.Inf
+                mx = -np.Inf
+                if index+lookahead >= length:
+                    break
+    
+    try:
+        if dump[0]:
+            max_peaks.pop(0)
         else:
-            if this > mn+delta:
-                mintab.append((mnpos, mn))
-                mx = this
-                mxpos = x[i]
-                lookformax = True
-
-    return maxtab, mintab
+            min_peaks.pop(0)
+        del dump
+    except ValueError:
+        pass
+        
+    return max_peaks, min_peaks
