c67f9323c09895e804a40553e35067f9d1336210
ana-balica <ana.balica@gmail.com>
2014-05-21 11:43:38 +0200

    implement extract_local_maxima() function

--- nosnore/core/dsp.py
+++ nosnore/core/dsp.py
@@ -60,6 +60,34 @@ def trim_psd(mags, freqs):
     return new_mags, freqs[:new_mags.size]
 
 
+def extract_local_maxima(mags, freqs):
+    """Get the peak values from some specific bins in PSD
+
+    :param mags: numpy 1D array magnitudes
+    :param freqs: numpy 1D array frequencies
+    :return: list of tuples (local_max_magniture, local_max_frequency)
+    """
+    freq_ranges = [(45, 55), (80, 90), (95, 105), (145, 155), (195, 205), (245, 255), (300, 310)]
+    local_maximas = []
+    for frange in freq_ranges:
+        lower_limit = frange[0]
+        upper_limit = frange[1]
+        indices = np.where((freqs > lower_limit) & (freqs < upper_limit))[0]
+        local_mags = []
+        for i in indices:
+            local_mags.extend([mags[i]])
+        local_mags = np.array(local_mags)
+        local_max_index = np.argmax(local_mags)
+        if local_max_index == indices[0] or local_max_index == indices[-1]:
+            local_maximas.extend([(0, 0)])
+        else:
+            local_max_mag = local_mags[local_max_index]
+            index = indices[local_max_index]
+            local_max_freq = freqs[index]
+            local_maximas.extend([(local_max_mag, local_max_freq)])
+    return local_maximas
+
+
 def envelope(signal):
     """Extract the envelope of the signal
 
