a789c1560f6d5adc7dbf6f18bde3a63ebc88c7a9
ana-balica <ana.balica@gmail.com>
2014-05-21 14:51:43 +0200

    split magnitudes in bins and compute the area by integrating each bin

--- nosnore/core/dsp.py
+++ nosnore/core/dsp.py
@@ -88,6 +88,27 @@ def extract_local_maxima(mags, freqs):
     return local_maximas
 
 
+def get_bin_areas(mags, freqs, window):
+    """Extract the area of integrating using composite trapezoidal rule
+
+    :param mags: numpy 1D array magnitudes
+    :param freqs: numpy 1D array frequencies
+    :param window: int length of the bin window
+    :return: list of area bins
+    """
+    bins = mags.size / window
+    bin_areas = []
+    bin_freqs = freqs[0:window]
+    j = 0
+    for i in xrange(bins):
+        try:
+            bin_areas.extend([np.trapz(mags[j:j+window], x=bin_freqs)])
+        except IndexError:
+            pass
+        j = i * window
+    return bin_areas
+
+
 def envelope(signal):
     """Extract the envelope of the signal
 
