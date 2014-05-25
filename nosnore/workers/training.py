53fc35b4d381bc771abf8772703f54f1d14a2433
ana-balica <ana.balica@gmail.com>
2014-05-25 17:28:59 +0200

    use 70% of the data for training

--- nosnore/workers/training.py
+++ nosnore/workers/training.py
@@ -2,18 +2,19 @@ import numpy as np
 from sklearn import linear_model
 from sklearn.preprocessing.label import LabelEncoder
 
-from nosnore.io.db import SnoreDatabase
-from nosnore.io.jsonio import LogisticRegressionJsonIO
+from nosnore.storage.db import SnoreDatabase
+from nosnore.storage.jsonio import LogisticRegressionJsonIO
 
 
 db = SnoreDatabase("pcp_features.db")
-samples_pos = db.get_features()
+samples_pos = db.get_features(limit=2500)
 samples_pos = [list(i[2:]) for i in samples_pos]
 db.db.close()
 
-db= SnoreDatabase("negative_features.db")
-samples_neg = db.get_features(len(samples_pos))
+db = SnoreDatabase("negative_features.db")
+samples_neg = db.get_features(limit=len(samples_pos))
 samples_neg = [list(i[2:]) for i in samples_neg]
+db.db.close()
 
 samples = samples_pos + samples_neg
 labels = [1]*len(samples_pos) + [0]*len(samples_neg)
