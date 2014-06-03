74ba6b6221c99b85360b88b74ceba2eb036df471
ana-balica <ana.balica@gmail.com>
2014-06-03 10:06:14 +0300

    app: basic UI and screen management

--- nosnoreapp/main.py
+++ nosnoreapp/main.py
@@ -1,15 +1,17 @@
 from kivy.app import App
 from kivy.config import Config
-from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.uix.screenmanager import ScreenManager, NoTransition
 
-
-class Panel(TabbedPanel):
-    pass
+from screens import SleepScreen, StatisticsScreen, SettingsScreen
 
 
 class NosnoreApp(App):
     def build(self):
-        return Panel()
+        self.manager = ScreenManager(transition=NoTransition())
+        self.manager.add_widget(SleepScreen(name='sleep'))
+        self.manager.add_widget(StatisticsScreen(name='statistics'))
+        self.manager.add_widget(SettingsScreen(name='settings'))
+        return self.manager
 
 
 if __name__ == '__main__':
---
+++ nosnoreapp/screens.py
@@ -0,0 +1,13 @@
+from kivy.uix.screenmanager import Screen
+
+
+class SleepScreen(Screen):
+    pass
+
+
+class StatisticsScreen(Screen):
+    pass
+
+
+class SettingsScreen(Screen):
+    pass
