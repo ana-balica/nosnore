d4adae92ad194e6bf6db3e548c1b4e538619695a
ana-balica <ana.balica@gmail.com>
2014-06-03 13:25:23 +0300

    app: bind start/stop button to change label

--- nosnoreapp/screens.py
+++ nosnoreapp/screens.py
@@ -2,7 +2,16 @@ from kivy.uix.screenmanager import Screen
 
 
 class SleepScreen(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(SleepScreen, self).__init__(**kwargs)
+        self.ids.toggle_rec_btn.bind(on_press=self.toggle_btn) 
+
+    def toggle_btn(self, *args):
+        btn_label = self.ids.toggle_rec_btn.text
+        if btn_label == "Start":
+            self.ids.toggle_rec_btn.text = "Stop"
+        else:
+            self.ids.toggle_rec_btn.text = "Start"
 
 
 class StatisticsScreen(Screen):
